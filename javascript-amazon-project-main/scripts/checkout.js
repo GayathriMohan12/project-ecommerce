3b52df1fbecccb9ecd8db5bc657d89b468e3f248
ecommerce <gayathrimohan8939@gmail.com>
2024-04-02 23:57:30 +0530

    made change completing exercise 15

--- javascript-amazon-project-main/scripts/checkout.js
+++
@@ -1,6 +0,0 @@
-import { renderOrderSummary } from "./checkout/ordersummary.js";
-import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-
-renderOrderSummary();
-renderPaymentSummary();
-
